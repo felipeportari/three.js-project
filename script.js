3e8d22f489bd7d910357edba107e62a1f0bdbfaf
felipeportari <felipe.portari7@gmail.com>
2025-03-29 18:42:25 -0300

    Atualizando script

--- script.js
+++ script.js
@@ -1,36 +1,98 @@
 import * as THREE from 'three';
+import ThreeGlobe from 'three-globe';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-const width = window.innerWidth, height = window.innerHeight;
+let renderer, camera, scene, controls, Globe;
+let mouseX = 0, mouseY = 0;
+let windowHalfX = window.innerWidth / 2;
+let windowHalfY = window.innerHeight / 2;
 
-// Inicialização da cena
-const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
-camera.position.z = 1;
+init();
+initGlobe();
+onWindowResize();
+animate();
 
-const scene = new THREE.Scene();
+function init() {
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(1); // Remove limitação da GPU
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+    document.body.appendChild(renderer.domElement);
 
-// Carregar textura
-const loader = new THREE.TextureLoader();
-const texture = loader.load('src/1.webp');
-texture.colorSpace = THREE.SRGBColorSpace;
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x040d21);
+    scene.fog = new THREE.Fog(0x535ef3, 400, 2000);
 
-// Criar esfera e aplicar textura
-const geometry = new THREE.SphereGeometry(0.2, 50, 50);
-const material = new THREE.MeshBasicMaterial({ map: texture });
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
+    camera.position.set(0, 0, 400);
+    scene.add(camera);
 
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+    let ambientLight = new THREE.AmbientLight(0xbbbbbb, 0.3);
+    scene.add(ambientLight);
 
-// Renderizador
-const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-renderer.setSize(width, height);
-document.body.appendChild(renderer.domElement);
+    let dLight = new THREE.DirectionalLight(0xFFFFFF, 0.8);
+    dLight.position.set(-800, 2000, 400);
+    camera.add(dLight);
 
-// Animação
-function animate(time) {
-    mesh.rotation.x = time / 2000;
-    mesh.rotation.y = time / 1000;
+    let dLight1 = new THREE.DirectionalLight(0x7982f6, 1);
+    dLight1.position.set(-200, 500, 200);
+    camera.add(dLight1);
 
-    renderer.render(scene, camera);
+    let dLight2 = new THREE.DirectionalLight(0x8566cc, 0.5);
+    dLight2.position.set(-200, 500, 200);
+    camera.add(dLight2);
+
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.enablePan = false;
+    controls.minDistance = 300;
+    controls.maxDistance = 500;
+    controls.rotateSpeed = 0.8;
+    controls.zoomSpeed = 1;
+    controls.autoRotate = false;
+    controls.minPolarAngle = Math.PI / 3.5;
+    controls.maxPolarAngle = Math.PI - Math.PI / 3;
+
+    window.addEventListener("resize", onWindowResize, false);
+    document.addEventListener("mousemove", onMouseMove);
 }
 
-renderer.setAnimationLoop(animate);
+function initGlobe() {
+    Globe = new ThreeGlobe({ waitForGlobeReady: true, animateIn: true })
+    .globeImageUrl('//unpkg.com/three-globe/example/img/earth-blue-marble.jpg');
+    Globe.rotateY(-Math.PI * (5 / 9));
+    Globe.rotateZ(-Math.PI / 6);
+
+    const globeMaterial = Globe.globeMaterial();
+    globeMaterial.color = new THREE.Color(0x3a228a);
+    globeMaterial.emissive = new THREE.Color(0x220038);
+    globeMaterial.emissiveIntensity = 0.1;
+    globeMaterial.shininess = 0.7;
+
+    scene.add(Globe);
+}
+
+function onMouseMove(event) {
+    mouseX = event.clientX - windowHalfX;
+    mouseY = event.clientY - windowHalfY;
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+    camera.position.x += (mouseX * 0.002 - camera.position.x) * 0.05;
+    camera.position.y += (-mouseY * 0.002 - camera.position.y) * 0.05;
+    camera.lookAt(scene.position);
+
+    Globe.rotation.y += 0.002;
+
+    if (controls.enabled) controls.update();
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate, renderer.domElement);
+}
